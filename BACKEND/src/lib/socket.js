bd97488a3e7ed1d15aff71d8638273fd4f34a3fe
kashish-2874 <kashishbarthwal2874@gmail.com>
2025-03-24 01:03:50 +0530

    changed urls backend

--- BACKEND/src/lib/socket.js
+++ BACKEND/src/lib/socket.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 
 const io = new Server(server,{
   cors : {
-    origin: "https://chat-application-8phfkxaso-kashishs-projects-389cba67.vercel.app",
+    origin: "https://chat-application-puce-tau.vercel.app",
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
     transports: ["websocket"],
