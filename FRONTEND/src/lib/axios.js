8f915a112bcf8676fcd9b3a8e924df35a50675c6
kashish-2874 <kashishbarthwal2874@gmail.com>
2025-05-08 11:21:02 +0530

    /

--- FRONTEND/src/lib/axios.js
+++ FRONTEND/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://chat-application-u5dm.onrender.com/api",
+  baseURL: "https://chat-application-u5dm.onrender.com/api/",
   withCredentials: true,
 });
